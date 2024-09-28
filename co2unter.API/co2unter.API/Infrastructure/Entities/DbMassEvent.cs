2edb0d8a522a15c300558312fcdacde3a955981f
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-28 22:14:53 +0200

    #2 add db mass event

---
+++ co2unter.API/co2unter.API/Infrastructure/Entities/DbMassEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+namespace co2unter.API.Infrastructure.Entities
+{
+    public class DbMassEvent
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public DateTimeOffset EventDate { get; set; }
+        public int EmmissionT { get; set; }
+
+        public DbMassEvent(Guid id, string name, string place, DateTimeOffset eventDate, int emmissionT)
+        {
+            Id = id;
+            Name = name;
+            Place = place;
+            EventDate = eventDate;
+            EmmissionT = emmissionT;
+        }
+
+        public DbMassEvent(string name, string place, DateTimeOffset eventDate, int EmmissionT)
+            => new DbMassEvent(Guid.NewGuid(), name, place, eventDate, EmmissionT);
+
+
+
+    }
+}
