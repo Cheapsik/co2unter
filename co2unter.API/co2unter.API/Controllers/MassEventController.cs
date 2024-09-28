583140d90511f436bc82048bf5c75b4a7aa98279
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-28 20:08:01 +0200

    add controller and model

---
+++ co2unter.API/co2unter.API/Controllers/MassEventController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace co2unter.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MassEventController : Controller
+    {
+        public MassEventController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ co2unter.API/co2unter.API/Models/MassEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace co2unter.API.Models
+{
+    public class MassEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset EventDate { get; set; }
+    }
+}
