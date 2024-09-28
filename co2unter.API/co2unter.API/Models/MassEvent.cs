52b28da42d9ee7247ba15dc6b37f31cbe074f538
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-28 21:34:27 +0200

    #2 mass events seeds data tmp

--- co2unter.API/co2unter.API/Models/MassEvent.cs
+++ co2unter.API/co2unter.API/Models/MassEvent.cs
@@ -4,6 +4,8 @@
     {
         public Guid? Id { get; set; }
         public string Name { get; set; }
+        public string Place { get; set; }
         public DateTimeOffset EventDate { get; set; }
+        public int EmmissionT {  get; set; }
     }
 }
--- co2unter.API/co2unter.API/Services/MassEventService.cs
+++ co2unter.API/co2unter.API/Services/MassEventService.cs
@@ -9,8 +9,9 @@ namespace co2unter.API.Services
 
         public MassEventService()
         {
-            
+            MassEventsSeed();   
         }
+
         public async Task<MassEvent?> GetMassEventByIdAsync(Guid massEventId)
         {
             return massEvents.FirstOrDefault(me => me.Id == massEventId);
@@ -32,6 +33,19 @@ namespace co2unter.API.Services
             massEvents.Add(massEvent);
 
             return massEvent.Id.Value;
+        }
+
+        private void MassEventsSeed()
+        {
+            massEvents.AddRange(new List<MassEvent>()
+            {
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now, Name = "XD1", EmmissionT = 100, Place = "Tama" },
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now, Name = "XD1_1", EmmissionT = 242, Place = "Bulwary" },
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now.AddDays(-2), Name = "XD2", EmmissionT = 10, Place = "Tauron Arena" },
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now.AddDays(-3), Name = "XD2_1", EmmissionT = 10, Place = "slumsy" },
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now.AddDays(-4), Name = "XD2_2", EmmissionT = 10, Place = "Stadion" },
+                new MassEvent() { Id = Guid.NewGuid(), EventDate = DateTime.Now.AddYears(-1), Name = "XD3", EmmissionT = 20, Place = "Tama" }
+            });
         }
     }
 }
