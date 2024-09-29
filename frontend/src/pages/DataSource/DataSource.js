7641840fc651cc0e65edcee697b8acfe384fadde
Cheapsik <cheapsik@gmail.com>
2024-09-29 10:39:39 +0200

    refactor: DataSource component

--- frontend/src/pages/DataSource/DataSource.js
+++ frontend/src/pages/DataSource/DataSource.js
@@ -4,13 +4,12 @@ const DataSource = () => {
     return (
         <div className="data-source">
             <div>
-                Data Source:
-                <a href="https://ztp.krakow.pl/" target="_blank">ZTP</a>
-                <a href="https://zzm.krakow.pl/" target="_blank">ZZM</a>
+                Źródła:
+                <a href="https://ztp.krakow.pl/" target="_blank" rel="noreferrer">ZTP</a>
+                <a href="https://zzm.krakow.pl/" target="_blank" rel="noreferrer">ZZM</a>
             </div>
-            <div className='special-section'>
-                <a href="https://instytutdanychzdupy.edu.pl/" target="_blank">IDzD</a>
-                <a href="https://www.facebook.com/IDzDPL/?locale=pl_PL" target="_blank">IDzDFb</a>
+            <div className='secret-section'>
+                <a href="https://instytutdanychzdupy.edu.pl/" target="_blank" rel="noreferrer">IDzD</a>
             </div>
         </div>
     );
