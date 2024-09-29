ed82628365fcb4c5853d5a099505ae4c8be87105
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-29 08:59:24 +0200

    add styles to source

--- frontend/src/pages/DataSource/DataSource.js
+++ frontend/src/pages/DataSource/DataSource.js
@@ -1,6 +1,8 @@
+import './DataSource.scss';
+
 const DataSource = () => {
     return (
-        <div className="calculator">
+        <div className="data-source">
             Data Source:
             <a href="https://ztp.krakow.pl/" target="_blank">ZTP</a>
             <a href="https://zzm.krakow.pl/" target="_blank">ZZM</a>
