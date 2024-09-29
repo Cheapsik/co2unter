792eb3c63370a4f504273e2493eaa57bac99241b
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-29 09:01:28 +0200

    add special section

--- frontend/src/pages/DataSource/DataSource.js
+++ frontend/src/pages/DataSource/DataSource.js
@@ -6,6 +6,10 @@ const DataSource = () => {
             Data Source:
             <a href="https://ztp.krakow.pl/" target="_blank">ZTP</a>
             <a href="https://zzm.krakow.pl/" target="_blank">ZZM</a>
+            <div className='special-section'>
+                <a href="https://instytutdanychzdupy.edu.pl/" target="_blank">IDzD</a>
+                <a href="https://www.facebook.com/IDzDPL/?locale=pl_PL" target="_blank">IDzDFb</a>
+            </div>
         </div>
     );
 }
