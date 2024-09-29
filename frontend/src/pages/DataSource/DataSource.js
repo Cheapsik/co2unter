bc9f43ca40bb5139bf2aed9511b759663192e18e
Atholn <mz3pr0gr4mmm3r@gmail.com>
2024-09-29 09:28:16 +0200

    refactor styles

--- frontend/src/pages/DataSource/DataSource.js
+++ frontend/src/pages/DataSource/DataSource.js
@@ -3,9 +3,11 @@ import './DataSource.scss';
 const DataSource = () => {
     return (
         <div className="data-source">
-            Data Source:
-            <a href="https://ztp.krakow.pl/" target="_blank">ZTP</a>
-            <a href="https://zzm.krakow.pl/" target="_blank">ZZM</a>
+            <div>
+                Data Source:
+                <a href="https://ztp.krakow.pl/" target="_blank">ZTP</a>
+                <a href="https://zzm.krakow.pl/" target="_blank">ZZM</a>
+            </div>
             <div className='special-section'>
                 <a href="https://instytutdanychzdupy.edu.pl/" target="_blank">IDzD</a>
                 <a href="https://www.facebook.com/IDzDPL/?locale=pl_PL" target="_blank">IDzDFb</a>
